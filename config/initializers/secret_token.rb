b40d01aa5d60e499d2abafbbc5c04191b490dc92
Kishore Eechambadi <keecham@gmail.com>
2013-03-25 16:13:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blacktie::Application.config.secret_token = '075ce2e2e5be3956d28fdfe76b79416faf964e221f880683b5fdb7984afaf73567a8a0bee47f4ccd02db22843ec9cae68116d801ff3d4a89642603cacbda0962'
